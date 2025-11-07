2c034ab985760a2e308b18847a09d89651bb4cad
Vikash <vikashvarnsolutions@gmail.com>
2025-11-08 01:05:34 +0530

    orthanc

--- server/auto-sync-simple.js
+++ server/auto-sync-simple.js
@@ -12,7 +12,7 @@ const Instance = require('./src/models/Instance');
 const Patient = require('./src/models/Patient');
 
 const REMOTE_ORTHANC = {
-    url: 'http://69.62.70.102:8042',
+    url: 'http://54.160.225.145:8042',
     username: 'orthanc',
     password: 'orthanc_secure_2024'
 };
